077333ee7eb099469267fb7c3bc6b4a8e3db054e
Frederic Simard <frederic-contractor@mobilitydata.org>
2025-01-29 12:51:12 -0500

    Added copying files to other languages

--- scripts/fetchdata.sh
+++ scripts/fetchdata.sh
@@ -4,4 +4,7 @@ version=v2.2
 
 curl https://raw.githubusercontent.com/MobilityData/gbfs/$version/gbfs.md -o docs/en/reference.md
 
+cp docs/en/reference.md docs/fr/reference.md
+cp docs/en/reference.md docs/es/reference.md
+
 # echo -e "\n\ntest" >> ../docs/*.md
